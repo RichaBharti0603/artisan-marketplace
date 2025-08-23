576100fb284f1157564f8605033390fce59e2e74
RichaBharti0603 <richab820@gmail.com>
2025-08-23 21:59:27 +0530

    menu works responsively

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import Marketplace from "./pages/Marketplace";
 import LoginSignup from "./pages/LoginSignup";
